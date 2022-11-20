8f8eea2f1fdf7fab31057c08f2ef13095aeba182
wlk <william.koelling@gmail.com>
2022-11-19 19:02:44 -0600

    MVP

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -12,14 +12,52 @@ export default function Work() {
         works.current?.scrollIntoView({ behavior: "smooth" })
     }
 
+    const projs = [
+        {
+            name : "Flareon Flip",
+            href : "https://github.com/wlk-dev/flareon-flip"
+        },
+        {
+            name : "Tech Blog",
+            href : "https://github.com/wlk-dev/tech-blog-site"
+        },
+        {
+            name : "Work Day Scheduler",
+            href : "https://github.com/wlk-dev/work-day-scheduler"
+        },
+        {
+            name : "Social Network API",
+            href : "https://github.com/wlk-dev/social-network-api"
+        },
+        {
+            name : "PWA Text Editor",
+            href : "https://github.com/wlk-dev/pwa-jate"
+        },
+        {
+            name : "Note Taker",
+            href : "https://github.com/wlk-dev/note-taker"
+        },
+        {
+            name : "Team Card Generator",
+            href : "https://github.com/wlk-dev/team-card-gen"
+        },
+        {
+            name : "Readme Generator (CLI)",
+            href : "https://github.com/wlk-dev/cli-readme-gen"
+        },
+        {
+            name : "Catify",
+            href : "https://github.com/wlk-dev/catify"
+        },
+    ]
 
     const createTiles = total => {
-        return Array.from(Array(total)).map((_, index) => {
-            return (<div key={index + 1} className="tile" idx={index}></div>)
+        return Array.from(projs).map((proj, index) => {
+            return (<a href={proj.href} target="_blank" key={index + 1} className="tile" idx={index}>{proj.name}</a>)
         })
     }
 
-    const tiles = createTiles(3 * 3)
+    const tiles = createTiles(6)
 
     return (
         <div id='tile-container'>
